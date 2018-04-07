c1f75acb1039687dc432bedf09c6bbac9f277cdb
Eric Cannon <ericcannon@Erics-MacBook-Pro-2.local>
2018-04-07 09:58:49 -0400

    Node Liri HW

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.1atV7Gt0SXUMBE8GA1JuyBXrH,
+  consumer_secret: process.env.DiO9iIKWyrh4gNGE7RGwfb4oBWvZxjIzqnjTdHnsfJc0ONUrxx,
+  access_token_key: process.env.982370926472949761-5OmCHWnmP9SSz8v8StKnkBVIhIXslDF,
+  access_token_secret: process.env.d6kyVwyuM0Ww4qguhXSrZTXOuyN1YZ8MSjwXKqn1ntD9h
+};
+
+exports.spotify = {
+  id: process.env.6b499c6ce8904dceb2badb3e82bb313c,
+  secret: process.env.1f456014ad904450b5f29b7982d7f53f
+};
